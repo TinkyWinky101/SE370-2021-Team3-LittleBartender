36745fcc29b1a381d929af67f072ad57b953ec84
Tyler Vo <tylervo02@yahoo.com>
2021-09-25 22:10:39 -0700

    Managed to push some work before I fall asleep

--- NonprojectStuff/CardShuffleProject/src/CardShuffle.java
+++ NonprojectStuff/CardShuffleProject/src/CardShuffle.java
@@ -1,13 +1,16 @@
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JButton;
+import java.awt.Color;
 
 public class CardShuffle extends JFrame {
     
     public CardShuffle(){
-        
+        JPanel greenBackground = new JPanel();
+        JButton shuffle = new JButton();
+
     }
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        new CardShuffle();
     }
 }
