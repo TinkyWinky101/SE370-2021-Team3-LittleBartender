26a97f3b18e5b47079eb640cfa04a521b6ab8107
Tyler Vo <tylervo02@yahoo.com>
2021-09-25 21:53:16 -0700

    Here are some not-code and some cards to work with.

---
+++ NonprojectStuff/CardShuffleProject/src/CardShuffle.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+
+public class CardShuffle extends JFrame {
+    
+    public CardShuffle(){
+        
+    }
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
