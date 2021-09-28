c29fc05d5fc57c3c29e06cc182c9c0f5ffd7fa12
Tyler Vo <tylervo02@yahoo.com>
2021-09-27 20:54:35 -0700

    Added a button and a green background. It needs to be darker. Sorry I couldn't do much this weekend.

--- NonprojectStuff/CardShuffleProject/src/CardShuffle.java
+++ NonprojectStuff/CardShuffleProject/src/CardShuffle.java
@@ -1,14 +1,18 @@
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JButton;
-import java.awt.Color;
+import javax.swing.*;
+import java.awt.*;
 
 public class CardShuffle extends JFrame {
-    
+    private JPanel greenBackGround;
     public CardShuffle(){
-        JPanel greenBackground = new JPanel();
+        greenBackGround = new JPanel();
         JButton shuffle = new JButton();
-
+        greenBackGround.setBackground(Color.green);
+        setSize(800, 900);
+        setLocationRelativeTo(null);
+        greenBackGround.add(shuffle);
+        add(greenBackGround, BorderLayout.CENTER);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
     }
     public static void main(String[] args) throws Exception {
         new CardShuffle();
