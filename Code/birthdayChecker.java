0aae2d08eecb8b54f302a70517421a6f01f0b9c7
Tyler Vo <tylervo02@yahoo.com>
2021-10-19 17:46:03 -0700

    updated birthdayChecker

--- Code/birthdayChecker.java
+++ Code/birthdayChecker.java
@@ -1,22 +1,28 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.util.*;
 
 public class birthdayChecker extends JPanel {
     private int month;
     private int day;
     private int year;
-    public final int CURRENT_YEAR = 2020;
-    
+    private boolean isLegalAdult = false;
+    public final static int CURRENT_YEAR = 2021;
+    public final int LEGAL_YEAR = 2000;
 
-    // private bool ifLegal(){
-    //     if(year >= 
-    // }
+    private boolean checkIfLegal(){
+        if(year <= LEGAL_YEAR){
+            return true;
+        }
+        return false;
+    }
 
     public static void main(String[] args){
         JFrame frame = new JFrame("A Simple GUI");
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // frame.setLayout(null);
         frame.setSize(500, 500);
         frame.setLocation(430, 100);
 
@@ -24,12 +30,16 @@ public class birthdayChecker extends JPanel {
 
         frame.add(panel);
 
-        JLabel lbl = new JLabel("Select one of the possible choices and click OK");
+        JLabel lbl = new JLabel("What is your birth year?");
         lbl.setVisible(true);
 
         panel.add(lbl);
 
-        String[] choices = { "CHOICE 1","CHOICE 2", "CHOICE 3","CHOICE 4","CHOICE 5","CHOICE 6"};
+        String[] choices = new String[50];
+        for(int i = 0; i < 50; i++){
+            int calculatedYear = CURRENT_YEAR - i;
+            choices[i] = String.valueOf(calculatedYear);
+        }
 
         final JComboBox<String> cb = new JComboBox<String>(choices);
 
